d1bf21ed7b30fbdc3ba3708ebb8bb49158bb7644
ronnyerlich <ronnyerlich2023@gmail.com>
2024-07-15 12:48:19 -0700

    Milestone 2

---
+++ assignment_manager.py
@@ -0,0 +1,115 @@
+from datetime import datetime, timedelta
+from assignment import Assignment
+import json
+
+
+class AssignmentManager:
+    """
+    Manages a dictionary of assignments
+    """
+
+    def __init__(self):
+        """
+        Initializes empty dictionary to manage assignments
+        """
+        self.assignments = {}
+
+    def add_assignment(self, assignment):
+        """
+        Adds a new assignment to the manager
+        """
+        self.assignments[assignment.name] = assignment
+        print(f"{assignment.name} added.")
+
+    def remove_assignment(self, assignment_name):
+        """
+        Removes assignment by name from the manager
+        """
+        if assignment_name in self.assignments:
+            del self.assignments[assignment_name]
+            print(f"{assignment_name} removed.")
+        else:
+            print(f"No assignment named {assignment_name}")
+
+    def print_sorted_assignments(self):
+        """
+        Prints all assignments, sorted by due date
+        """
+        sorted_assignments = sorted(
+            self.assignments.values(), key=lambda x: x.get_due()
+        )  # Sort assignments by due date
+        print("\nUpcoming Assignments:\n")
+        if not sorted_assignments:
+            print("No upcoming assignments!")
+        for assignment in sorted_assignments:
+            print(f"{assignment.name}: {assignment.get_due_str()}")
+
+    def print_weeks_assignments(self):
+        """
+        Prints assignments that are due within the upcoming week
+        """
+        sorted_assignments = sorted(
+            self.assignments.values(), key=lambda x: x.get_due()
+        )  # Sort assignments by due date
+        week_end = datetime.now().date() + timedelta(
+            days=7
+        )  # Calculate 1 week from now
+        week_assignments = []
+        print("\nUpcoming Week's Assignments:\n")
+
+        for assignment in sorted_assignments:
+            if (
+                datetime.now().date() <= assignment.due_date <= week_end
+            ):  # If within week, add assignment to week_assignments
+                week_assignments.append(assignment)
+                print(
+                    f"{assignment.name}: {assignment.get_due_str()}"
+                )
+        if not week_assignments:
+            print("No assignments this week!\n")
+
+    def change_name(self, assignment, new_name):
+        """
+        Changes the name of an assignment
+        """
+        old_name = assignment.name
+        assignment.set_name(new_name)
+        print(f"Name for {old_name} changed to {new_name}.")
+
+    def change_due_date(self, assignment, new_due_date):
+        """
+        Changes the due date of an assignment
+        """
+        old_date = assignment.due_date
+        assignment.set_due_date(new_due_date)
+        print(f"Due date for {old_date} changed to {new_due_date}.")
+
+    def change_due_time(self, assignment, new_due_time):
+        """
+        Changes the due time of an assignment
+        """
+        old_time = assignment.due_time
+        assignment.set_due_time(new_due_time)
+        print(f"Due time for {old_time} changed to {new_due_time}.")
+
+    def find_assignment(self, assignment_name):
+        """
+        Finds an assignment by name
+        """
+        return self.assignments.get(assignment_name, None)
+
+    def save_to_json(self, filename):
+        assignments_dict = {}
+        for assignment in self.assignments.values():
+            assignments_dict[assignment.name] = assignment.to_dictionary()
+            
+        with open(filename, "w") as f:
+            json.dump(assignments_dict, f, indent = 2)
+            
+    
+    def load_from_json(self, saved_assignments):
+        with open("saved_assignments.json", "r") as json_file:
+            data = json.load(json_file)
+            for assignment_dict in data.values():
+                assignment = Assignment(assignment_dict["name"], assignment_dict["due_date"], assignment_dict["due_time"])
+                self.assignments[assignment.name] = assignment
---
+++ cli.py
@@ -0,0 +1,64 @@
+from assignment import Assignment
+from assignment_manager import AssignmentManager
+
+
+def main():
+    """
+    Runs command-line interface to manage assignments
+    """
+    tracker = AssignmentManager()
+    tracker.load_from_json("saved_assignments.json")
+    while True:
+        print(
+            "\n 1. Add Assignment\n 2. Delete Assignment\n 3. Modify Assignment \n 4. Sort by Due Date\n 5. View the week's assignments\n 6. Quit\n"
+        )
+        choice = input("What would you like to do? ")
+        choice = int(choice)
+        if choice == 1:
+            # Add a new assignment
+            name = input("Give this assignment a name: ")
+            due_date = input("When is it due? ")
+            due_time = input("What time is it due? ")
+            tracker.add_assignment(Assignment(name, due_date, due_time))
+        elif choice == 2:
+            # Delete an assignment
+            tracker.print_sorted_assignments()
+            to_remove = input("Which assignment would you like to delete? ")
+            tracker.remove_assignment(to_remove)
+        elif choice == 3:
+            # Modify an assignment's name, due date, or due time
+            modify = input(" 1. Change Name\n 2. Change Due Date\n 3. Change Due Time ")
+            tracker.print_sorted_assignments()
+            assignment_name = input("Which assignment do you want to change? ")
+            assignment = tracker.find_assignment(assignment_name)
+            if assignment:
+                modify = int(modify)
+                if modify == 1:
+                    new_name = input("New assignment name? ")
+                    tracker.change_name(assignment, new_name)
+                elif modify == 2:
+                    new_date = input("New due date? ")
+                    tracker.change_due_date(assignment, new_date)
+                elif modify == 3:
+                    new_time = input("New due time? ")
+                    tracker.change_due_time(assignment, new_time)
+                else:
+                    print("Invalid input: please enter 1, 2, or 3. \n")
+            else:
+                print(f"No assignment named {assignment_name}")
+        elif choice == 4:
+            # Print all assignments sorted by due date
+            tracker.print_sorted_assignments()
+        elif choice == 5:
+            # Prints assignments that are due within the upcoming week
+            tracker.print_weeks_assignments()
+        elif choice == 6:
+            tracker.save_to_json("saved_assignments.json")
+            # Exit the program
+            break
+        else:
+            print("Invalid input: please enter a number between 1 and 5. \n")
+
+
+if __name__ == "__main__":
+    main()
